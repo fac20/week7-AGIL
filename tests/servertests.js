8256007db779af5dc3aa0ee2d4bb5d0dd87b2f00
Amber <a.rignell16@ejm.org>
2020-08-26 16:34:57 +0100

    Create server tests
    Co-authored-by: Jessica <j.r.deacon@hotmail.co.uk>
    
    Relates #9

---
+++ tests/servertests.js
@@ -0,0 +1,34 @@
+const test = require("tape");
+const router = require("./router")
+const supertest = require("supertest");
+
+test("Initialise (check tests are running properly", t => {
+    let num = 2;
+    t.equal(num, 2, "Should return 2");
+    t.end();
+  });
+
+test("check status code is 200 and / take you to home page", t => {
+    supertest(router)
+        .get("/")
+        .expect(200)
+        .expect("Content-Type", "text/plain")
+        .end((err, res) => {
+            t.error(err);
+            t.equal(res.text.includes('travel'), true);
+        });
+});
+
+
+test("check status code is 404", t => {
+    supertest(router)
+        .get("/nonexistenturl")
+        .expect(404)
+        .expect("content-type", "text/html")
+        .end((err, res) => {
+            t.error(err);
+            t.equal(res.text, "<h1>Not Found</h1>");
+            t.end();
+        });
+});
+
